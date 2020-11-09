0c96f4c89f63525123c2aad97032b43bb2b6dbce
Adrian Barriento <adri.bace@gmail.com>
2020-11-10 00:30:56 +0100

    ultimos retoques

--- src/main/java/gestion_bbdd/GestionarBBDD.java
+++ src/main/java/gestion_bbdd/GestionarBBDD.java
@@ -7,11 +7,10 @@ import java.sql.SQLException;
 public class GestionarBBDD {
     public void insertarRegistros(int numRegistros, int numHilos){
         Connection conexionBD;
-        int registrosXhilo = numRegistros/numHilos, diferencia =0;
-        //así calcularemos si se queda algún registro fuera en caso de que el número de registros no sea divisible entre el número de hilos pedidos. Diferencia está inicializada a 0 por si la operación es divisible.
-        if((numRegistros%numHilos)!=0){
-            diferencia = numRegistros-(registrosXhilo*numHilos);
-        }
+        int registrosXhilo = numRegistros/numHilos;
+        //calculamos si se queda algún registro fuera en caso de que el número de registros no sea divisible entre el número de hilos pedidos
+        int diferencia = numRegistros-(registrosXhilo*numHilos);
+
         //conectamos a la base de datos e insertamos tantos registros en x hilos
         try{
             conexionBD = DriverManager.getConnection("jdbc:mysql://localhost/bbdd_psp_1", "DAM2020_PSP", "DAM2020_PSP");
